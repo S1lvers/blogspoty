752bf5287b1ea70f4ac32499bf05f85ac40af17c
S1lvers <anton.gribushka@gmail.com>
2019-09-28 08:00:58 +0300

    Сделал java приложение

---
+++ src/main/java/com/blogfusion/Application.java
@@ -0,0 +1,27 @@
+package com.blogfusion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.blogfusion"})
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        this.onStartup();
+    }
+
+    void onStartup() {
+        System.out.println("\n\n\t+=====================================+");
+        System.out.println("\t|   STARTING SBERDATA WEB APPLICATION   |");
+        System.out.println("\t+=====================================+\n\n");
+    }
+}
